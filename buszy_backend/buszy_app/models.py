f53ddc96b5e52528b77638c57c0fe629dadd50d0
Yusuf Emre Muştu <yusufemre.mustu@agu.edu.tr>
2025-04-18 19:18:52 +0300

    voyages table models added

--- buszy_backend/buszy_app/models.py
+++ buszy_backend/buszy_app/models.py
@@ -45,3 +45,88 @@ class User(models.Model):
                 return user.check_password(raw_password)
             else:
                 return False
+
+
+
+
+class Voyage(models.Model):
+    bus_id = models.AutoField(primary_key=True)
+    bus_company = models.CharField(max_length=50)
+    bus_plate = models.CharField(max_length=10)
+    seats_emp = models.JSONField()
+    seats_full = models.JSONField()
+    crew = models.CharField(max_length=100)
+    cities = models.JSONField()  # JSON veri tipi ile şehirleri saklamak için
+
+
+    @staticmethod
+    def create_voyage(bus_company,bus_plate,crew,cities):
+        query="""
+                INSERT INTO voyage (bus_company, bus_plate, seats_emp,seats_full,crew,cities)
+                VALUES (%s,%s,%s,%s,%s,%s)
+        """
+        from django.db import connection
+        with connection.cursor() as cursor:
+            cursor.execute(query,[bus_company,bus_plate,crew,cities])
+
+    @staticmethod
+    def select_voyage(bus_id):
+        query= """
+            SELECT * FROM voyage WHERE bus_id = %d
+        """        
+        from django.db import connection
+        with connection.cursor() as cursor:
+            cursor.execute(query,[bus_id])
+            voyage_data = cursor.fetchone()
+
+        if voyage_data:
+            return voyage_data
+        else:
+            return None        
+
+
+
+
+    def __str__(self):
+        return f"{self.bus_company} ({self.bus_plate})"
+
+    class Meta:
+        db_table = 'voyage'  # Veritabanındaki tablo adı  
+
+
+
+class VoyageListing(models.Model):
+    bus_company = models.CharField(max_length=50)
+    bus_time = models.TimeField()
+    bus_list_begin = models.CharField(max_length=20)
+    bus_list_end = models.CharField(max_length=20)
+    bus_list_price = models.DecimalField(max_digits=4, decimal_places=2)
+    bus_id = models.ForeignKey(Voyage, on_delete=models.CASCADE, related_name='voyage')  # FOREIGN KEY ile ilişki
+
+    def __str__(self):
+        return f"{self.bus_company} - {self.bus_time}"
+
+    class Meta:
+        db_table = 'voyage_listing'  # Veritabanındaki tablo adı
+
+
+
+    #VOYAGES TABLE
+    #bus_id
+    #firma
+    #destinations
+    #bus_plaka
+    #uygun_koltuklar
+    #dolu_koltuklar
+    #crew_driver
+
+
+    #VOYAGES LISTING
+    #voyages_listing_id
+    #firma
+    #saat
+    #where
+    #towhere
+    #price
+    #bus_id (foreign key)
+
