2b40ca1f1eac258511ad12fb2872841abe7d8570
Yusuf Serhat Pamuk <yusufserhatpamuk7@gmail.com>
2025-04-21 11:59:35 +0300

    voyage_listing added

---
+++ frontend/app/voyage_listings/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const VoyageListingPage = () => {
+  return(
+  <>
+    <Navbar></Navbar>
+    
+    <Footer></Footer>
+  </>);
+};
+
+export default VoyageListingPage;
